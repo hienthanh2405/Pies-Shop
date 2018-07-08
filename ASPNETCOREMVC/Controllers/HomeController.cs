b901f47ab7035a6142933c50b756a5c6542918d4
My-PC\My <hienthanh2405@gmail.com>
2018-07-08 14:28:37 +0700

    Initial_project

---
+++ ASPNETCOREMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using ASPNETCOREMVC.Models;
+using ASPNETCOREMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace ASPNETCOREMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie overview";
+
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Pies = pies.ToList(),
+                Title = "Welcome to Bethany's Pie Shop"
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
+    }
+}
---
+++ ASPNETCOREMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETCOREMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+
+    }
+}
